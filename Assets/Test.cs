b9b9fb101b1e84eb00bbfa7ab17c588455df8e52
Tomo1-hiro <75882793+Tomo1-hiro@users.noreply.github.com>
2020-12-25 18:46:30 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < array.Length; i++)
+            Debug.Log(array[i]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int i = 4; i < array.Length; i--)
+            Debug.Log(array[i]);
+        
+
+    }
+    }

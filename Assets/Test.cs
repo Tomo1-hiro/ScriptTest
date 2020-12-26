b70a5dce8a8b3b2898bb7993f19700f654bf2693
Tomo1-hiro <75882793+Tomo1-hiro@users.noreply.github.com>
2020-12-26 16:46:55 +0900

    全てvoid.starteでの記入
    全てんのコードをvoid.starte内で記入

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,15 +9,21 @@ public class Test: MonoBehaviour
     {
         int[] array = { 1, 2, 3, 4, 5 };
         for (int i = 0; i < array.Length; i++)
+        {
             Debug.Log(array[i]);
+        }
+        for (int i = 4; i < array.Length && i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        int[] array = { 1, 2, 3, 4, 5 };
-        for (int i = 4; i < array.Length; i--)
-            Debug.Log(array[i]);
+        
         
 
     }

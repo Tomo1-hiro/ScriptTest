855d809538552eea43be09a1f58416abd1d3849b
Tomo1-hiro <75882793+Tomo1-hiro@users.noreply.github.com>
2021-01-10 22:33:40 +0900

    Lesson4 発展問題
    Lesson4　発展問題の提出

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,29 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test: MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public class Boss
+    {
+
+        int mp = 53;
+        public void Magic()
+        {
+            if (mp >= 5)
+            {
+                mp -= 5;
+                Debug.Log("魔法攻撃をした。残りMPは" + mp);
+
+            }
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない。");
+            }
+
+        }          
+                   
+            
+
+        }
+    
     void Start()
     {
-        int[] array = { 1, 2, 3, 4, 5 };
-        for (int i = 0; i < array.Length; i++)
+        Boss lastboss = new Boss();
+        for (int i = 0; i < 11; i++)
         {
-            Debug.Log(array[i]);
+            lastboss.Magic();
         }
-        for (int i = 4; i < array.Length && i >= 0; i--)
-        {
-            Debug.Log(array[i]);
-        }
+    }
+
 
     }
+
+   
     
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        
-
-    }
-    }
